5723b30f4ee1ffdb312aef40e716a57bc6711e3e
sollo11 <userl@40a401eb-531f-47bc-8d66-08013b5a8700.NIC.c.cc>
2021-02-15 11:13:21 +0100

    Revert "Initial commit"
    This reverts commit 826b0086e5343c8dbe7412337a63763e43bbc927.

--- pc@p.c/diagnostics/system/Device/RS_WindowsUpdate.ps1
+++
@@ -1,7 +0,0 @@
-# Copyright © 2008, Microsoft Corporation. All rights reserved.
-
-. .\CL_Utility.ps1
-
-Get-DiagInput -ID "IT_ChangeDeviceInstallationSettings"
-WaitForInstall
-
--- pc@p.c/diagnostics/system/PCW/VF_ProgramCompatibilityWizard.ps1
+++
@@ -1,5 +0,0 @@
-# Copyright © 2008, Microsoft Corporation. All rights reserved.
-
-
-#RS_ProgramCompatibilityWizard
-#rparsons - 05 May 2008
