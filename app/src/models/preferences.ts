94c5aab5408386bb27a237156a9379ff7e5d59b3
NIC ccc <Neuromancer@outlook.cz>
2021-02-17 20:34:06 +0100

    Signed-off-by: NIC ccc <Neuromancer@outlook.cz>

--- app/src/models/preferences.ts
+++
@@ -1,8 +0,0 @@
-export enum PreferencesTab {
-  Accounts = 0,
-  Integrations = 1,
-  Git = 2,
-  Appearance = 3,
-  Prompts = 4,
-  Advanced = 5,
-}
--- app/src/ui/octicons/index.ts
+++
@@ -1,5 +0,0 @@
-export { Octicon } from './octicon'
-export { OcticonSymbol } from './octicons.generated'
-export { iconForRepository } from './repository'
-export { iconForStatus } from './status'
-export { syncClockwise } from './sync-clockwise'
